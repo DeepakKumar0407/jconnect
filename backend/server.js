c798099361ef9f10cd326a8356b989334920372a
Deepak Kumar <deepak.kumar016211@gmail.com>
2026-01-23 18:03:45 +0530

    add basic socket io fratures

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,44 @@
 import express from "express"
+import { createServer } from "node:http"
 import mongoose from "mongoose"
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
 import commentRoutes from "./routes/commentRoutes.js"
 import notificationRoutes from "./routes/notificationRoutes.js"
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from "./routes/authRoutes.js"
 import bodyParser from "body-parser"
-import cookieParser from 'cookie-parser'
-import cloudinary from "./cloudinaryConfig.js";
-import 'dotenv/config'
-import cors from "cors";
+import cookieParser from "cookie-parser"
+import cloudinary from "./cloudinaryConfig.js"
+import "dotenv/config"
+import cors from "cors"
+import { Server } from "socket.io"
 
 const app = express()
+const server = createServer(app)
 app.use(express.json())
-app.use(express.text());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.text())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
 const port = process.env.PORT
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  },
+  connectionStateRecovery: {}
+})
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
-}));
+}))
 
-const ConnectDb = async()=> {
+const ConnectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
-    dbName:'jconnect'
-})
-  console.log("DB connected")
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "jconnect"
+    })
+    console.log("DB connected")
   } catch (error) {
     console.log(error)
   }
@@ -36,12 +46,22 @@ const ConnectDb = async()=> {
 
 await ConnectDb()
 
-app.use("/users",userRoutes)
-app.use("/posts",postRoutes)
-app.use("/comments",commentRoutes)
-app.use("/notifications",notificationRoutes)
-app.use("/auth",authRoutes)
+io.on("connection", (socket) => {
+  console.log("user connected")
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg)
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
+})
 
-app.listen(port,()=>{
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
+app.use("/comments", commentRoutes)
+app.use("/notifications", notificationRoutes)
+app.use("/auth", authRoutes)
+
+server.listen(port, () => {
   console.log(`listining at port ${port}`)
 })
--- frontend/src/pages/ChatRoom.tsx
+++ frontend/src/pages/ChatRoom.tsx
@@ -1,6 +1,57 @@
+import { useEffect, useRef, useState } from "react"
+import { io, Socket } from "socket.io-client"
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid"
+
 const ChatRoom = () => {
+  const socketRef = useRef<Socket | null>(null)
+  const [yaxis, setYAxis] = useState(window.innerHeight)
+  const [chat, setChat] = useState<string[]>([])
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000", {
+      autoConnect: false
+    })
+  }, [])
+  useEffect(() => {
+    const updateHeight = () => setYAxis(window.innerHeight)
+
+    updateHeight()
+    window.addEventListener("resize", updateHeight)
+
+    return () => window.removeEventListener("resize", updateHeight)
+  }, [])
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    socketRef.current?.emit('chat message', message)
+    setMessage('')
+  }
+  useEffect(() => {
+    const socket = socketRef.current
+    if (!socket) return
+
+    socket.on("chat message", (msg) => {
+      setChat(state => [...state, msg])
+    })
+    socket.connect()
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
   return (
-    <div className="div">ChatRoom</div>
+    <div className="div" style={{ height: `${yaxis}px` }}>
+      <div className="flex flex-col justify-between pb-22 h-full">
+        <div className="overflow-auto w-full">{chat.map((m, index: number) => (
+        <p key={index}>{m}</p>
+      ))}
+      </div>
+      <form onSubmit={handleSubmit} className="w-full">
+        <label htmlFor="chat" className=" hidden lg:inline md:text-xl lg:text-2xl">Chat: </label>
+        <input id="chat" type="text" name="chat" value={message} placeholder="text" className="p-3 border-2 border-white/20 rounded w-3/4 lg:w-1/2 align-bottom" onChange={(e) => setMessage(e.target.value)} required></input>
+        <button className="ml-2 p-2 border-2 border-green-600 rounded"><PaperAirplaneIcon className="icon text-green-600"/></button>
+      </form>
+      </div>
+    </div>
   )
 }
 export default ChatRoom
